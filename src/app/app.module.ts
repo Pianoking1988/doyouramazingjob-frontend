77f8cf7813747d02a452beecf00ce1c0a71bcba1
Oliver Heinemann <Oliver Heinemann>
2017-02-17 17:46:42 +0100

    Add first components for routing test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { JobListComponent } from './components/job-list/job-list.component';
+
+const appRoutes: Routes = [
+  { path: '',        component: UserListComponent },
+  { path: 'jobs', component: JobListComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UserListComponent,
+    JobListComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
